05e3a5984dca2d7d824e28cf0a78669461cfb511
Philip Lee <leephili@usc.edu>
2015-10-16 16:03:57 -0400

    Tested service.py on local ubuntu.

--- Philip/service.py
+++ Philip/service.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
-from bottle import route, run
+from bottle import route, run, request
 import os
+import time
 from time import strftime
 import decipher
+import string
 
 # Set time zone to Pittsburgh
 os.environ['TZ'] = 'US/Eastern'
@@ -11,10 +13,12 @@ time.tzset()
 
 @route('/q1')
 def process():
-	params = []
-	message = decipher.decipher()
+	key = request.query.key
+	cipher = request.query.message
+	result = decipher.decipher(["decipher", key, cipher])
 	time = strftime("%Y-%m-%d %H:%M:%S")
-	page = "Team Entropy, 846185807052\n" + time + "\n" + message + "\n"
+	page = "Team Entropy, 846185807052\n" + str(time) + "\n" + str(result) + "\n"
 	return page
 
 run(host='localhost', port=80)
+# run(port=80) for real attempt
